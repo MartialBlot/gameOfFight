5ccbcd541582adfa582a12e351d4308e89574f33
BLOT Martial <martial.blot@outlook.fr>
2019-07-03 14:28:01 +0200

    first mouvements Goku OK

--- index.js
+++ index.js
@@ -13,18 +13,20 @@ let dY = 0;
 
 let goku = new Image();
 goku.src = "assets/pictures/GokuSuperSaiyan.png";
-let gokuWidth = 720; 
-let gokuHeight = 220; 
+let gokuWidth = 2400; 
+let gokuHeight = 165; 
 let gokuRows = 1; 
-let gokuCols = 6; 
+let gokuCols = 20; 
 let gWidth = gokuWidth/gokuCols;  
 let gHeight = gokuHeight/gokuRows; 
 let gokuCurFrame = 0; 
-let gokuFrameCount = 6; 
+let gokuFrameCount = 20; 
 let gokuX=200;
-let gokuY=370; 
+let gokuY=270; 
 let gokuSrcX= 0; 
-let gokuSrcY= 510;
+let gokuSrcY= 565;
+//Mise en place posInit après intro au combat
+setTimeout(posInitRightGoku, 2385)
 
 let ctx = canvas.getContext("2d");
 
@@ -41,18 +43,90 @@ document.addEventListener('keydown',function(e){
     keyState[e.keyCode || e.which] = true;
 },true);    
 document.addEventListener('keyup',function(e){
-    keyState[e.keyCode || e.which] = false;},true);
+    keyState[e.keyCode || e.which] = false;
+},true);
+
+function gameLoop() {
     
-    function gameLoop() {
-        
-        setTimeout(gameLoop, 10);
-    }    
-    gameLoop();
-
-    function draw(){
-        updateFrame();
-        ctx.drawImage(map,bX,bY,741,300,dX,dY,1200,800);
-        ctx.drawImage(goku,gokuSrcX,gokuSrcY,gokuWidth,gokuHeight,gokuX,gokuY,gokuWidth,gokuHeight);
+    if (keyState[87]){
+        gokuY--
+        if(gokuY===270){
+            posInitRightGoku()
+        }
+        if(gokuY<270){
+            posFlightRightGoku()
+        }
     }
+    
+    if (keyState[83] && gokuY<270){
+        gokuY++
+        if(gokuY===270){
+            posInitRightGoku()
+        }
+        if(gokuY<270){
+            posFlightRightGoku()
+        }
+    }
+    
+    if(keyState[190]){
+        rightPunch();
+    }
+    
+    setTimeout(gameLoop, 10);
+}    
+gameLoop();
 
-    setInterval(draw, 80);
+function draw(){
+    updateFrame();
+    ctx.drawImage(map,bX,bY,741,300,dX,dY,1200,800);
+    ctx.drawImage(goku,gokuSrcX,gokuSrcY,gWidth,gHeight,gokuX,gokuY,gWidth*1.3,gHeight*1.3);
+}
+
+setInterval(draw, 110);
+
+function posInitRightGoku(){
+    gokuWidth = 1200; 
+    gokuHeight = 165; 
+    gokuRows = 1; 
+    gokuCols = 10;
+    gWidth = gokuWidth/gokuCols;  
+    gHeight = gokuHeight/gokuRows; 
+    gokuFrameCount = 10; 
+    gokuSrcY= 400;
+}
+
+function posFlightRightGoku(){
+    gokuWidth = 480; 
+    gokuHeight = 165; 
+    gokuRows = 1; 
+    gokuCols = 4;
+    gWidth = gokuWidth/gokuCols;  
+    gHeight = gokuHeight/gokuRows; 
+    gokuFrameCount = 4; 
+    gokuSrcY= 940;
+}
+
+function rightPunch(){
+    if(gokuY===270){
+        gokuWidth = 600; 
+        gokuHeight = 150; 
+        gokuRows = 1; 
+        gokuCols = 4;
+        gWidth = gokuWidth/gokuCols;  
+        gHeight = gokuHeight/gokuRows; 
+        gokuFrameCount = 4; 
+        gokuSrcY= 755;
+        setTimeout(posInitRightGoku, 250)
+    }
+    if(gokuY<270){
+        gokuWidth = 905; 
+        gokuHeight = 180; 
+        gokuRows = 1; 
+        gokuCols = 7;
+        gWidth = gokuWidth/gokuCols;  
+        gHeight = gokuHeight/gokuRows; 
+        gokuFrameCount = 7; 
+        gokuSrcY= 1160;
+        setTimeout(posFlightRightGoku, 500)
+    }
+}
